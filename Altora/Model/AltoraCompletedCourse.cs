4ce6c31ed6bdfb58b8fde0679bce202a6c1ef545
Andre Ferreira <AFerreira@PFWork.com.au>
2025-09-17 15:07:22 +1000

    Remove old files

--- Altora/Model/AltoraCompletedCourse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Altora.Model
-{
-    public class AltoraCompletedCourse
-    {
-        public string? UserId { get; set; }
-        public string? IssueDate { get; set; }
-        public string? ExpiryDate { get; set; }
-    }
-}
